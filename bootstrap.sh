74b52ac22d91e9e95fb45f9fa1532d72531e0446
Dimitris Geromichalos <geromidg@gmail.com>
2017-08-16 12:45:42 +0200

    Fix path in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ rm -rf ~/.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+~/.vim/bundle/youcompleteme/install.py --clang-completer
 
 for file in $home_files; do
   ln -svf $PWD/$file ~/.$file
